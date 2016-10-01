e71255c620a2354416710cb119c6665cde88f77d
Spencer Plant <splant@ualberta.ca>
2016-10-01 12:59:24 -0600

    added scripts to rooms and their coordinates

--- I Hate That Guy/Assets/Scripts/Room/Room.cs
+++ I Hate That Guy/Assets/Scripts/Room/Room.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
+[Serializable]
 public class Room : MonoBehaviour {
+
+    public int x;
+    public int y;
 
 	// Use this for initialization
 	void Start () {
