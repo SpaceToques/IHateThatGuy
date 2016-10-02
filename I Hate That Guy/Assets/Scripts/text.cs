e0fed9e4b502f9e10dfd8c99ce791907bd10b97d
Tim Tran <ttran1@ualberta.ca>
2016-10-02 14:21:43 -0600

    Added a UI and aliens talking

---
+++ I Hate That Guy/Assets/Scripts/text.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class text : MonoBehaviour {
+
+    Text info;
+
+	// Use this for initialization
+	void Start () {
+        info = GetComponent<Text>();
+        info.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void alienVoice()
+    {
+        info.text = "Spooky Voice: We ArE cOmInG foR yUoOOoOOoO";
+    }
+}
