4ce2305412b35334e8cfd20cf57a7ad42d46bebb
tommy3ua <tommy3@ualberta.ca>
2016-10-02 16:09:38 -0600

    speech bubble

---
+++ I Hate That Guy/Assets/Scripts/Actors/Danny/DannySpeech.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DannySpeech : MonoBehaviour {
+
+    // 0. create empty gameobject and attach this script
+    // 1. create canvas and assign to script
+    // 2. create text as child of canvas and assign to script
+    // 3. assign danny to script
+
+    public GameObject danny;
+    public RectTransform text;
+    public RectTransform canvas;
+
+    private GameObject room;
+
+    // Use this for initialization
+    void Start () {
+	}
+
+    void Update()
+    {
+        Vector3 dannyScreenPoint = Camera.main.WorldToScreenPoint(danny.transform.position);
+        Vector2 localPointOnCanvasRect;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas, dannyScreenPoint, null, out localPointOnCanvasRect);
+        localPointOnCanvasRect.x = localPointOnCanvasRect.x+330;
+        localPointOnCanvasRect.y = localPointOnCanvasRect.y + 175;
+        text.position = localPointOnCanvasRect;
+    }
+}
