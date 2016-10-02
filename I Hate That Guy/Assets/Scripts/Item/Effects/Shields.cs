2ea51b2658329dc8b0ea0e46c48721d328d3ac0b
Alex <alex.sampley@gmail.com>
2016-10-02 10:42:30 -0600

    Added shields that can be disabled

---
+++ I Hate That Guy/Assets/Scripts/Item/Effects/Shields.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shields : GameStateListener {
+    public override void shieldsDown(bool shieldsDown) {
+        // hide or show based on shield status
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = !shieldsDown;
+    }
+}
--- I Hate That Guy/Assets/Scripts/Item/Interactables/ShieldsButton.cs
+++ I Hate That Guy/Assets/Scripts/Item/Interactables/ShieldsButton.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System;
 
-public class Shields : Interactable {
+public class ShieldsButton : Interactable {
     protected override void InteractAction(GameObject interactor) {
         if (interactor.GetComponent<Ghost>() != null) {
             gameState.shieldsDown = true;
