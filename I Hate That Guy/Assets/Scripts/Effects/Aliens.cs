dc4d236be111f160fcc1628febe9ae3043aa81f4
Tim Tran <ttran1@ualberta.ca>
2016-10-02 12:33:44 -0600

    beams

--- I Hate That Guy/Assets/Scripts/Effects/Aliens.cs
+++ I Hate That Guy/Assets/Scripts/Effects/Aliens.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class Aliens : GameStateListener {
 
     public GameObject laserPrefab;
+    public GameObject damageParticlesPrefab;
     private GameObject lasers;
+    private GameObject damageParticles;
 
     public override void aliensMad(bool aliensMad) {
         if (aliensMad) {
             Debug.Log("1000 Aliens cry out in anger!");
+
             //instantiate the lasers as a child of the aliens
-            lasers = Instantiate(laserPrefab) as GameObject;
+            lasers = Instantiate(laserPrefab, gameObject.transform.position, laserPrefab.transform.rotation) as GameObject;
+            lasers.transform.SetParent(gameObject.transform);
+
+            //instantiate the damage particles in front of the ship
+            damageParticles = Instantiate(damageParticlesPrefab, damageParticlesPrefab.transform.position, damageParticlesPrefab.transform.rotation) as GameObject;
         }
 
         // make lasers visible if aliens are mad
